8b5d85cbfc1b763b7bcaefdec9c20172229d3dc4
Yisheng Cai <yisheng.cai@adp.com>
2019-01-16 16:10:58 -0500

    feature: add download to get svgs

--- dist/fetch.js
+++ dist/fetch.js
@@ -12,10 +12,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
+const download_1 = __importDefault(require("download"));
 const fetchDom = () => __awaiter(this, void 0, void 0, function* () {
     return axios_1.default.get('https://material.io/tools/icons/?style=round', { responseType: 'json' })
         .then(console.log)
         .catch(console.warn);
 });
-fetchDom();
+const iterateNames = () => __awaiter(this, void 0, void 0, function* () {
+    const fs = require('fs');
+    const file = fs.readFileSync('./icon-names', 'utf-8');
+    const result = file.split('\n');
+    console.log('Download start, please wait');
+    Promise.all(result.map((iconName) => {
+        const url = `https://material.io/tools/icons/static/icons/round-${iconName}-24px.svg`;
+        const destination = './icon-files';
+        return new Promise((resolve, reject) => {
+            download_1.default(url, destination).then(() => { resolve(); }).catch(reject);
+        });
+    })).then(() => {
+        console.log('Download complete');
+    });
+});
+iterateNames();
 //# sourceMappingURL=fetch.js.map
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import download from 'download';
+import { promises } from 'fs';
 
 const fetchDom = async () => {
   return axios.get('https://material.io/tools/icons/?style=round', { responseType: 'json' })
@@ -6,4 +8,25 @@ const fetchDom = async () => {
     .catch(console.warn);
 };
 
-fetchDom();
+const iterateNames = async () => {
+  const fs = require('fs');
+  const file = fs.readFileSync('./icon-names', 'utf-8');
+  const result = file.split('\n');
+
+  console.log('Download start, please wait');
+
+  Promise.all(result.map((iconName : string) => {
+    const url = `https://material.io/tools/icons/static/icons/round-${iconName}-24px.svg`;
+    const destination = './icon-files';
+
+    return new Promise((resolve, reject) => {
+      download(url, destination).then(() => { resolve(); }).catch(reject);
+    })
+  })).then(() => {
+    console.log('Download complete');
+  }).catch((e) => {
+    console.log('An error occured', e);
+  });
+}
+
+iterateNames();
