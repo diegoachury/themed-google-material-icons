9f5427e2a370583b6945dd68aec51a699f9e3f05
Yisheng Cai <yisheng.cai@adp.com>
2019-01-16 22:49:27 -0500

    fix: add fallback for if the default size icon returns 404

--- src/download.ts
+++ src/download.ts
@@ -1,7 +1,9 @@
 import download from 'download';
 
+const baseUrl = 'https://material.io/tools/icons/static/icons';
 const theme = 'round';
 const size = 24;
+const fallbackSize = 18;
 
 const iterateNames = async () => {
   const fs = require('fs');
@@ -11,11 +13,21 @@ const iterateNames = async () => {
   console.log('Download start, please wait');
 
   Promise.all(result.map((iconName : string) => {
-    const url = `https://material.io/tools/icons/static/icons/${theme}-${iconName}-${size}px.svg`;
+    const url = `${baseUrl}/${theme}-${iconName}-${size}px.svg`;
     const destination = './icon-files';
 
     return new Promise((resolve, reject) => {
-      download(url, destination).then(() => { resolve(); }).catch(reject);
+      return download(url, destination, { filename: iconName })
+        .then(() => { resolve(); })
+        .catch((e) => {
+          console.warn('Failed to download a specific icon.', e, 'Trying a different size for this');
+          return download(`${baseUrl}/${theme}-${iconName}-${fallbackSize}px.svg`)
+            .then(() => {
+              console.log(`Download with fallback size ${fallbackSize}px was successful`);
+              resolve();
+            })
+            .catch(reject);
+        });
     })
   })).then(() => {
     console.log('Download complete');
